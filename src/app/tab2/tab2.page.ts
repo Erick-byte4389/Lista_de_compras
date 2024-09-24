555554fddb5fa8c46ca80f7be150a7e62c451447
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-23 22:33:14 -0600

    Añadiendo alertas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ShoppingItemsService } from '../services/shopping-items.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,10 +11,38 @@ export class Tab2Page {
 
   public item: string;
 
-  constructor() {}
+  constructor(private shoppingList: ShoppingItemsService, private alertController: AlertController) {}
 
   addItem() {
     console.log(this.item);
+
+    if(!this.shoppingList.existItem(this.item)){
+      this.shoppingList.addItem(this.item);
+      this.item = '';
+      console.log(this.shoppingList.items);
+      this.alertSucces();
+    } else {
+      this.alertError();
+    }
+
+  }
+
+  async alertSucces(){
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'El artículo se ha agregado correctamente',
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+
+  async alertError(){
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'El artículo ya existe en la lista',
+      buttons: ['OK']
+    })
+    await alert.present();
   }
 
 }
