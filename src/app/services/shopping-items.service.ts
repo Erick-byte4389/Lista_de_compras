7a2ad8c0d0ca1dc826b41f8bb83cbdfba4ccedfa
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-23 20:14:53 -0600

    Creando logica de items

--- src/app/services/shopping-items.service.ts
+++ src/app/services/shopping-items.service.ts
@@ -14,4 +14,30 @@ export class ShoppingItemsService {
     this.isEmpty = true;
 
   }
+
+  addItem(item:string){
+    this.items.push(item);
+    this.isEmpty = false;
+  }
+
+  removeItem(item: string){
+    let index = this.items.findIndex(it => it.toUpperCase().trim() === item.toUpperCase().trim());
+    if (index != -1) {
+      this.items.splice(index, 1);
+      if(this.items.length == 0){
+        this.isEmpty = true;
+      }
+    }
+  }
+
+  removeAllItems(){
+    this.items = [];
+    this.isEmpty = true;
+  }
+
+  existItem(item: string){
+    const itemFound = this.items.find(it => it.toUpperCase().trim() === item.toUpperCase().trim());
+    return itemFound;
+  }
+
 }
