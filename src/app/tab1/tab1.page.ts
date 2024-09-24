def0e57bdda6a468a2df6ee1789dbba18ed2ea5f
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-23 23:20:13 -0600

    Logica de eliminar item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ShoppingItemsService } from '../services/shopping-items.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,28 @@ import { ShoppingItemsService } from '../services/shopping-items.service';
 })
 export class Tab1Page {
 
-  constructor(public shoppingList: ShoppingItemsService) {}
+  constructor(public shoppingList: ShoppingItemsService, private alertController: AlertController) {}
 
-}
+  async removeItem(item) {
+
+    const alert = await this.alertController.create({
+      header: 'Eliminar artículo',
+      message: `¿Desea eliminar el artículo ${item}?`,
+      buttons: [
+        {
+          text: 'Sí',
+          handler: () => {
+            this.shoppingList.removeItem(item);
+          }
+        },
+        {
+          text: 'No',
+          handler: () => {
+            alert.dismiss();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+}  
